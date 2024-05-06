0a44fd4fbaedfdc37c7d9ea5d20dd09d357267f6
Sudharshan G K <Sudharshangk7702@gmail.com>
2024-05-06 14:23:42 +0530

    Adding Folders

---
+++ AngularAuthAPI/AngularAuthAPI/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AngularAuthPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace AngularAuthPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+    }
+}
---
+++ AngularAuthAPI/AngularAuthAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AngularAuthPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
